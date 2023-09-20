76703283c5fd46004e0b9494a49aa66effa73927
Akki1104 <ak947314@gmail.com>
2023-09-20 10:55:29 +0530

    File Added

---
+++ JavaPracticals/src/insertion.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class insertion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("Enter size of list: ");
+        n = sc.nextInt();
+        int []a = new int[n];
+        System.out.println("Enter the elements in the list");
+        int i;
+        for(i = 0; i<n; i++){
+            System.out.print("Elements: ");
+            a[i] = sc.nextInt();
+        }
+
+        System.out.print("List: ");
+        for(int j: a)
+            System.out.print(j+" ");
+        System.out.println();
+
+        System.out.println("How many elements you want to insert in the list? ");
+        int x= sc.nextInt();
+        System.out.println("From which position you want to insert: ");
+        int p = sc.nextInt();
+
+        a = new int[n+x];
+
+        System.out.println("Enter the elements: ");
+        for(i = p-1; i<x; i++){
+            a[i + x] = a[i];
+            a[i] = sc.nextInt();
+        }
+        for(int j: a)
+            System.out.println(j);
+    }
+}
---
+++ sorting.java
@@ -0,0 +1,29 @@
+[33mcommit a4a14c34962753eabd96ef7ddd9498b29f66fe40[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Akki1104 <ak947314@gmail.com>
+Date:   Wed Sep 20 10:49:40 2023 +0530
+
+    File Added
+
+[33mcommit a7906902b765f3b65f7dd2bb5e3a1c9abc59f549[m
+Author: Akki1104 <ak947314@gmail.com>
+Date:   Tue Sep 19 12:03:46 2023 +0530
+
+    File Added
+
+[33mcommit 119a5db987551b28a14c46d35c2a6207f5e9ef70[m
+Author: Akki1104 <ak947314@gmail.com>
+Date:   Mon Sep 18 23:26:36 2023 +0530
+
+    File Added
+
+[33mcommit 07a66c7e8dc7b1bc2a42f9e579d5bd5a226bdd4a[m
+Author: Akash Kumar <121248893+Akki1104@users.noreply.github.com>
+Date:   Thu Sep 14 21:14:38 2023 +0530
+
+    Create README.md
+
+[33mcommit 5d753dd8ae3d3430dc3739d8aaea73f76818644a[m
+Author: Akki1104 <ak947314@gmail.com>
+Date:   Thu Sep 14 21:08:04 2023 +0530
+
+    File Added
